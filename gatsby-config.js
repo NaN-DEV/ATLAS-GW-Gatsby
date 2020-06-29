9ed63fd06a2a2360f93d41d9bf883e2dc8620a07
NaN <nan@MacBook-Air-NaN.local>
2020-06-29 16:07:01 +0200

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,7 +18,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/assets/images`,
+        path: `${__dirname}/src/assets/`,
       },
     },
     {
--- src/templates/service.js
+++ src/templates/service.js
@@ -4,7 +4,6 @@ import Section from "../components/organisms/section/section"
 const Service = props => {
   const index = props.pageContext.index
   const content = props.pageContext.content
-
   return (
     <>
       <Section full service index={props.pageContext.index} />
